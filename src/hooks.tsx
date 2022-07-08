ab58c82f0eb54e3d191af3bd37211a5b497d67b7
Anwesh43 <anweshthecool0@gmail.com>
2022-07-08 10:48:19 +0530

    adding useStyle hook

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, CSSProperties } from "react";
 
 const scGap : number = 0.01 
 const delay : number = 20 
@@ -37,12 +37,44 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
     return {
         w, 
         h 
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const size : number = Math.min(w, h) / 10 
+    const position = 'absolute'
+    return {
+        parentStyle() : CSSProperties {
+            const left = `${w / 2}px`
+            const top = `${h / 2}px`
+            return {
+                position, 
+                left, 
+                top  
+            }
+        },
+        blockStyle(i : number) : CSSProperties {
+            const left = `${-size / 2 + (w / 2 - size / 2) * scale * (1 - 2 * i)}px`
+            const top = `${-size / 2}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            const background = 'indigo'
+            return {
+                width, 
+                height, 
+                left, 
+                top, 
+                background,
+                position 
+            }
+        },
+
     }
 }
